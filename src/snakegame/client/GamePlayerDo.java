6c6340a16c2ae9e19cf712a0528cedab6ef7eec8
pibi9265 <pibi9265@outlook.com>
2019-05-10 11:15:12 +0900

    add client

---
+++ src/snakegame/client/GamePlayerDo.java
@@ -0,0 +1,10 @@
+package snakegame.client;
+
+import snakegame.Board;
+
+public class GamePlayerDo{
+    public static void main(String[] args) {
+		Board board = new Board();
+		board.init();
+	}
+}
